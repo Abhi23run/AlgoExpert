f89cee6540b1d50b00f8b1270819790c8bd98a8c
Abhinav Arun <abhinavarun23@gmail.com>
2023-10-05 14:40:26 -0400

    Also added my O(1) memory in-place solution

--- arrays/move_element_to_end.py
+++ arrays/move_element_to_end.py
@@ -11,3 +11,22 @@ def moveElementToEnd(array, toMove):
     m=len(final_list)
     final_list+=[toMove]*(n-m)
     return final_list
+
+
+def moveElementToEnd(array, toMove):
+    # Write your code here.
+    ## O(1) memory -> in-memory solution
+    l,r=0,len(array)-1
+    while(l<=r):
+      if((array[l]==toMove) & (array[r]!=toMove)):
+        array[l],array[r]=array[r],array[l]
+        l+=1
+        r-=1
+      elif((array[l]!=toMove) & (array[r]==toMove)):
+        l+=1
+      elif((array[l]==toMove) & (array[r]==toMove)):
+        r-=1
+      else:
+        l+=1
+
+    return array
